94b328bb0bb88149c44956a89df6602f8ae4dc50
flying-dp <flying19941115@163.com>
2021-11-20 17:12:14 +0800

    init

---
+++ src/test/java/com/example/shengdemo/ShengDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shengdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShengDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
